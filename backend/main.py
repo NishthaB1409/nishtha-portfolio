4421e61eb5601e7f80e66a6ff417843818ae26f9
Nishtha Bhagyawant <nishthabhagyawant@gmail.com>
2026-03-31 20:11:39 +0200

    Final

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-app = FastAPI()   # 👈 THIS LINE IS CRITICAL
+app = FastAPI()   
 
 app.add_middleware(
     CORSMiddleware,
